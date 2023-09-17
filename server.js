271ff7ce3c42b0c5456613da64b78b341ecea9df
rahulchopra-dev <rahulchopra.dev@gmail.com>
2023-09-17 07:19:55 +0530

    update port

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/convert-to-pdf', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
