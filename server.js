8f15eccdd547cb8addb3a93b18f557bb3e0114f5
rahulchopra-dev <rahulchopra.dev@gmail.com>
2023-09-17 07:44:45 +0530

    use pdfkit for pdf generation, remove puppeteer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
-const { Readable } = require('stream'); // Import the Readable stream
+const PDFDocument = require('pdfkit');
+const htmlToPdf = require('html-pdf'); // Import the html-pdf library
+const { Readable } = require('stream');
 const app = express();
 
 app.use(express.json());
@@ -13,25 +14,25 @@ app.post('/convert-to-pdf', async (req, res) => {
       return res.status(400).json({ error: 'HTML content is required in JSON format.' });
     }
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(html);
+    // Convert HTML to PDF using html-pdf
+    htmlToPdf.create(html).toBuffer((err, pdfBuffer) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'PDF generation error.' });
+      }
 
-    const pdfBuffer = await page.pdf();
+      // Set the response headers
+      res.setHeader('Content-Disposition', 'attachment; filename=converted.pdf');
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Length', pdfBuffer.length);
 
-    await browser.close();
+      // Use a Readable stream to send PDF data in chunks
+      const stream = new Readable();
+      stream.push(pdfBuffer);
+      stream.push(null);
 
-    // Set the response headers
-    res.setHeader('Content-Disposition', 'attachment; filename=converted.pdf');
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Length', pdfBuffer.length); // Set content length
-
-    // Use a Readable stream to send PDF data in chunks
-    const stream = new Readable();
-    stream.push(pdfBuffer);
-    stream.push(null); // Signal the end of the stream
-
-    stream.pipe(res);
+      stream.pipe(res);
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred.' });
